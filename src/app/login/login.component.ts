ec399188958cfeeefa05088ed76c239da25c3072
AJRuiz2204 <ajruiz2204@gmail.com>
2024-04-23 19:22:19 -0600

    First commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component, ElementRef, VERSION, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+
+
+
+export class LoginComponent {
+  isActive: boolean = false;
+
+  togglePanel(){
+    this.isActive=!this.isActive;
+  }
+}
+
+
